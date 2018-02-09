420c38ecc0ec2c68886d9295125cc80fd98c7044
Richard Igbiriki <imorobebh@gmail.com>
2018-02-09 06:44:23 +0100

    UI fixes to offline survey

--- enque/components/surveyPage1.js
+++ enque/components/surveyPage1.js
@@ -23,7 +23,144 @@ import SurveyPage2 from './surveyPage2'
 import Header from './header'
 var key = 'AIzaSyCRBOQE2ZcuttQDxreNI1BbxBMDbX0XGEo'
 Translation.setConfig(ProviderTypes.Google, key,'ig')
-
+var sdgImages = [
+  {
+    key:1,
+    image: require ('../assets/images/1_Poverty_SDG.png'),
+    selected:false,
+    color:'#EB1C2E',
+    title:'Poverty',
+    description:'End poverty in all its forms everywhere'
+ },
+  {
+    key:2,
+    image: require ('../assets/images/2_ZeroHunger.jpg'),
+    selected:false,
+    color:'#DDA73A',
+    title:'Zero Hunger',
+    description:'End hunger, achieve food security and improved nutrition and promote sustainable agriculture'
+  },
+  {
+    key:3,
+    image: require ('../assets/images/3_GoodHealthCare.jpg'),
+    selected:false,
+    color:'#49A341',
+    title: 'Good Health and Well-Being',
+    description:'Ensure healthy lives and promote well-being for all at all ages'
+  },
+  {
+    key:4,
+    image: require ('../assets/images/4_Education.png'),
+    selected:false,
+    color:'#C21F32',
+    title:'Quality Education',
+    description:'Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all'
+  },
+  {
+    key:5,
+    image: require ('../assets/images/5_Gender.jpg'),
+    selected:false,
+    color:'#EF402D',
+    title:'Gender Equality',
+    description:'Achieve gender equality and empower all women and girls'
+  },
+  {
+    key:6,
+    image: require ('../assets/images/6_CleanWater.png'),
+    selected:false,
+    color:'#01AED9',
+    title:'Clean Water and Sanitation',
+    description:'Ensure availability and sustainable management of water and sanitation for all'
+  },
+  {
+    key:7,
+    image: require ('../assets/images/7_Energy.jpg'),
+    selected:false,
+    color:'#FBC412',
+    title:'Affordable and Clean Energy',
+    description:'Ensure access to affordable, reliable, sustainable and modern energy for all'
+  },
+  {
+    key:8,
+    image: require ('../assets/images/8_DecentWork.png'),
+    selected:false,
+    color:'#8F1838',
+    title:'Decent Work & Economic Growth',
+    description:'Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all'
+  },
+  {
+    key:9,
+    image: require ('../assets/images/9_Industry.png'),
+    selected:false,
+    color:'#F36D24',
+    title:'Industry, Innovation, and Infrastructure',
+    description:'Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation'
+  },
+  {
+    key:10,
+    image: require ('../assets/images/10_ReducedEquality.jpg'),
+    selected:false,
+    color:'#DE1768',
+    title:'Reduced Inequalities',
+    description:'Reduce inequality within and among countries'
+  },
+  {
+    key:11,
+    image: require ('../assets/images/11_SustainableCities.png'),
+    selected:false,
+    color:'#FA9D26',
+    title:'Sustainable Cities and Communities',
+    description:'Make cities and human settlements inclusive, safe, resilient and sustainable'
+  },
+  {
+    key:12,
+    image: require ('../assets/images/12_Recycle.png'),
+    selected:false,
+    color:'#CD8D29',
+    title:'Responsible Consumption & Production',
+    description:'Ensure sustainable consumption and production patterns'
+  },
+  {
+    key:13,
+    image: require ('../assets/images/13_Climate.png'),
+    selected:false,
+    color:'#47773D',
+    title:'Climate Action',
+    description:'Take urgent action to combat climate change and its impacts',
+  },
+  {
+    key:14,
+    image: require ('../assets/images/14_LifeBelowWeater.jpg'),
+    selected:false,
+    color:'#1F97D4',
+    title:'Life Below Water',
+    description:'Conserve and sustainably use the oceans, seas and marine resources for sustainable development'
+  },
+  {
+    key:15,
+    image: require ('../assets/images/15_LifeOnLand.png'),
+    selected:false,
+    color:'#3DB04B',
+    title:'Life on Land',
+    description:'Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss'
+  },
+  {
+    key:16,
+    image: require ('../assets/images/16_Justice.png'),
+    selected:false,
+    color:'#025589',
+    title:'Peace, Justice and Strong Institutions',
+    description:'Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels'
+  },
+  {
+    key:17,
+    image: require ('../assets/images/17_Parnerships.png'),
+    selected:false,
+    color:'#183668',
+    title:'Partnerships for the Goals',
+    description:'Strengthen the means of implementation and revitalize the global partnership for sustainable development'
+  }
+];
 export default class SurveyPage1 extends Component<{}> {
   constructor (props) {
     super (props)
@@ -83,7 +220,7 @@ export default class SurveyPage1 extends Component<{}> {
         <Header title='New Survey' backButton={true} noAdd={true} />
         <View style={styles.secondaryContainer}>
           {this.state.showNext ?
-          <SurveyPage2 status={this.state.status} /> : this.showPage1 () }
+          <SurveyPage2 images={sdgImages} status={this.state.status} /> : this.showPage1 () }
         </View>
       </View>
     );
--- enque/components/surveyPage2.js
+++ enque/components/surveyPage2.js
@@ -31,150 +31,149 @@ var radio_props = [
   {label: '4', value: 4 },
   {label: '5', value: 5 },
 ];
-const sdgImages = [
-  {
-    key:1,
-    image: require ('../assets/images/1_Poverty_SDG.png'),
-    selected:false,
-    color:'#EB1C2E',
-    title:'Poverty',
-    description:'End poverty in all its forms everywhere'
- },
-  {
-    key:2,
-    image: require ('../assets/images/2_ZeroHunger.jpg'),
-    selected:false,
-    color:'#DDA73A',
-    title:'Zero Hunger',
-    description:'End hunger, achieve food security and improved nutrition and promote sustainable agriculture'
-  },
-  {
-    key:3,
-    image: require ('../assets/images/3_GoodHealthCare.jpg'),
-    selected:false,
-    color:'#49A341',
-    title: 'Good Health and Well-Being',
-    description:'Ensure healthy lives and promote well-being for all at all ages'
-  },
-  {
-    key:4,
-    image: require ('../assets/images/4_Education.png'),
-    selected:false,
-    color:'#C21F32',
-    title:'Quality Education',
-    description:'Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all'
-  },
-  {
-    key:5,
-    image: require ('../assets/images/5_Gender.jpg'),
-    selected:false,
-    color:'#EF402D',
-    title:'Gender Equality',
-    description:'Achieve gender equality and empower all women and girls'
-  },
-  {
-    key:6,
-    image: require ('../assets/images/6_CleanWater.png'),
-    selected:false,
-    color:'#01AED9',
-    title:'Clean Water and Sanitation',
-    description:'Ensure availability and sustainable management of water and sanitation for all'
-  },
-  {
-    key:7,
-    image: require ('../assets/images/7_Energy.jpg'),
-    selected:false,
-    color:'#FBC412',
-    title:'Affordable and Clean Energy',
-    description:'Ensure access to affordable, reliable, sustainable and modern energy for all'
-  },
-  {
-    key:8,
-    image: require ('../assets/images/8_DecentWork.png'),
-    selected:false,
-    color:'#8F1838',
-    title:'Decent Work & Economic Growth',
-    description:'Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all'
-  },
-  {
-    key:9,
-    image: require ('../assets/images/9_Industry.png'),
-    selected:false,
-    color:'#F36D24',
-    title:'Industry, Innovation, and Infrastructure',
-    description:'Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation'
-  },
-  {
-    key:10,
-    image: require ('../assets/images/10_ReducedEquality.jpg'),
-    selected:false,
-    color:'#DE1768',
-    title:'Reduced Inequalities',
-    description:'Reduce inequality within and among countries'
-  },
-  {
-    key:11,
-    image: require ('../assets/images/11_SustainableCities.png'),
-    selected:false,
-    color:'#FA9D26',
-    title:'Sustainable Cities and Communities',
-    description:'Make cities and human settlements inclusive, safe, resilient and sustainable'
-  },
-  {
-    key:12,
-    image: require ('../assets/images/12_Recycle.png'),
-    selected:false,
-    color:'#CD8D29',
-    title:'Responsible Consumption & Production',
-    description:'Ensure sustainable consumption and production patterns'
-  },
-  {
-    key:13,
-    image: require ('../assets/images/13_Climate.png'),
-    selected:false,
-    color:'#47773D',
-    title:'Climate Action',
-    description:'Take urgent action to combat climate change and its impacts',
-  },
-  {
-    key:14,
-    image: require ('../assets/images/14_LifeBelowWeater.jpg'),
-    selected:false,
-    color:'#1F97D4',
-    title:'Life Below Water',
-    description:'Conserve and sustainably use the oceans, seas and marine resources for sustainable development'
-  },
-  {
-    key:15,
-    image: require ('../assets/images/15_LifeOnLand.png'),
-    selected:false,
-    color:'#3DB04B',
-    title:'Life on Land',
-    description:'Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss'
-  },
-  {
-    key:16,
-    image: require ('../assets/images/16_Justice.png'),
-    selected:false,
-    color:'#025589',
-    title:'Peace, Justice and Strong Institutions',
-    description:'Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels'
-  },
-  {
-    key:17,
-    image: require ('../assets/images/17_Parnerships.png'),
-    selected:false,
-    color:'#183668',
-    title:'Partnerships for the Goals',
-    description:'Strengthen the means of implementation and revitalize the global partnership for sustainable development'
-  }
-];
-export default class SurveyPage2 extends Component<{}> {
+export default class SurveyPage2 extends Component {
   constructor (props) {
     super (props)
     this.state = {
       translated:'',
-      images:sdgImages,
+      images:[
+        {
+          key:1,
+          image: require ('../assets/images/1_Poverty_SDG.png'),
+          selected:false,
+          color:'#EB1C2E',
+          title:'Poverty',
+          description:'End poverty in all its forms everywhere'
+       },
+        {
+          key:2,
+          image: require ('../assets/images/2_ZeroHunger.jpg'),
+          selected:false,
+          color:'#DDA73A',
+          title:'Zero Hunger',
+          description:'End hunger, achieve food security and improved nutrition and promote sustainable agriculture'
+        },
+        {
+          key:3,
+          image: require ('../assets/images/3_GoodHealthCare.jpg'),
+          selected:false,
+          color:'#49A341',
+          title: 'Good Health and Well-Being',
+          description:'Ensure healthy lives and promote well-being for all at all ages'
+        },
+        {
+          key:4,
+          image: require ('../assets/images/4_Education.png'),
+          selected:false,
+          color:'#C21F32',
+          title:'Quality Education',
+          description:'Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all'
+        },
+        {
+          key:5,
+          image: require ('../assets/images/5_Gender.jpg'),
+          selected:false,
+          color:'#EF402D',
+          title:'Gender Equality',
+          description:'Achieve gender equality and empower all women and girls'
+        },
+        {
+          key:6,
+          image: require ('../assets/images/6_CleanWater.png'),
+          selected:false,
+          color:'#01AED9',
+          title:'Clean Water and Sanitation',
+          description:'Ensure availability and sustainable management of water and sanitation for all'
+        },
+        {
+          key:7,
+          image: require ('../assets/images/7_Energy.jpg'),
+          selected:false,
+          color:'#FBC412',
+          title:'Affordable and Clean Energy',
+          description:'Ensure access to affordable, reliable, sustainable and modern energy for all'
+        },
+        {
+          key:8,
+          image: require ('../assets/images/8_DecentWork.png'),
+          selected:false,
+          color:'#8F1838',
+          title:'Decent Work & Economic Growth',
+          description:'Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all'
+        },
+        {
+          key:9,
+          image: require ('../assets/images/9_Industry.png'),
+          selected:false,
+          color:'#F36D24',
+          title:'Industry, Innovation, and Infrastructure',
+          description:'Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation'
+        },
+        {
+          key:10,
+          image: require ('../assets/images/10_ReducedEquality.jpg'),
+          selected:false,
+          color:'#DE1768',
+          title:'Reduced Inequalities',
+          description:'Reduce inequality within and among countries'
+        },
+        {
+          key:11,
+          image: require ('../assets/images/11_SustainableCities.png'),
+          selected:false,
+          color:'#FA9D26',
+          title:'Sustainable Cities and Communities',
+          description:'Make cities and human settlements inclusive, safe, resilient and sustainable'
+        },
+        {
+          key:12,
+          image: require ('../assets/images/12_Recycle.png'),
+          selected:false,
+          color:'#CD8D29',
+          title:'Responsible Consumption & Production',
+          description:'Ensure sustainable consumption and production patterns'
+        },
+        {
+          key:13,
+          image: require ('../assets/images/13_Climate.png'),
+          selected:false,
+          color:'#47773D',
+          title:'Climate Action',
+          description:'Take urgent action to combat climate change and its impacts',
+        },
+        {
+          key:14,
+          image: require ('../assets/images/14_LifeBelowWeater.jpg'),
+          selected:false,
+          color:'#1F97D4',
+          title:'Life Below Water',
+          description:'Conserve and sustainably use the oceans, seas and marine resources for sustainable development'
+        },
+        {
+          key:15,
+          image: require ('../assets/images/15_LifeOnLand.png'),
+          selected:false,
+          color:'#3DB04B',
+          title:'Life on Land',
+          description:'Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss'
+        },
+        {
+          key:16,
+          image: require ('../assets/images/16_Justice.png'),
+          selected:false,
+          color:'#025589',
+          title:'Peace, Justice and Strong Institutions',
+          description:'Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels'
+        },
+        {
+          key:17,
+          image: require ('../assets/images/17_Parnerships.png'),
+          selected:false,
+          color:'#183668',
+          title:'Partnerships for the Goals',
+          description:'Strengthen the means of implementation and revitalize the global partnership for sustainable development'
+        }
+      ],
       selectedImages:[],
       value:0,
     }
@@ -183,7 +182,6 @@ export default class SurveyPage2 extends Component<{}> {
     this.feedbacks = []
   }
   componentWillMount () {
-    this.setState({images:sdgImages})
     this.checkInternet()
   }
   async checkInternet () {
@@ -261,20 +259,24 @@ export default class SurveyPage2 extends Component<{}> {
         </View>
     )
   }
+  listFooter = () => {
+    return (<Button style={styles.submit} textStyle={{fontSize: 18, color:'white'}} onPress={()=>this.setState({selectionDone:true})}>NEXT</Button>)
+  }
   showList () {
     return (
       <View style={{flex:1}}>
         <Text style={styles.question}>WHICH SIX OF THE FOLLOWING GLOBAL GOALS ARE OF IMMEDIATE CONCERN TO YOU AND YOUR FAMILY?</Text>
-        <View>
           <FlatList
             data={this.state.images}
             renderItem={this.renderItem}
             extraData={this.state}
+            ListFooterComponent={this.listFooter}
           />
-          <Button style={styles.submit} textStyle={{fontSize: 18, color:'white'}} onPress={()=>this.setState({selectionDone:true})}>NEXT</Button>
-        </View>
       </View>
     )
+  }
+  renderFooter = () => {
+    return (<Button style={styles.submit} textStyle={{fontSize: 18, color:'white'}} onPress={()=>this.saveTemp()}>Continue</Button>)
   }
   showQuestions () {
     return (
@@ -287,8 +289,8 @@ export default class SurveyPage2 extends Component<{}> {
             renderItem={this.renderSelected}
             extraData={this.state}
             style={{marginBottom:10}}
+            ListFooterComponent={this.renderFooter}
           />
-          <Button style={styles.submit} textStyle={{fontSize: 18, color:'white'}} onPress={()=>this.saveTemp()}>Continue</Button>
         </View>
       </View>
     )
@@ -374,9 +376,7 @@ export default class SurveyPage2 extends Component<{}> {
   render () {
     return (
       <View style={styles.container}>
-        <ScrollView style={{flex:1, marginTop:20}}>
-          {this.state.selectionDone ? this.showQuestions() : this.showList() }
-        </ScrollView>
+        {this.state.selectionDone ? this.showQuestions() : this.showList() }
       </View>
 
     );
@@ -386,6 +386,7 @@ export default class SurveyPage2 extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop:20,
   },
   question: {
     fontSize: 20,
